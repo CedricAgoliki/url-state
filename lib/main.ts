ce228f8cc7d2708c25353a15ba5e0fc3d165482e
Cédric Agoliki <agolikicedric@gmail.com>
2023-10-07 11:37:37 +0000

    Changed implementation to a class and added seperator feature

--- lib/main.ts
+++ lib/main.ts
@@ -1,22 +1,39 @@
-function set(state: Map<string, string>): void {
-  const searchParams = new URLSearchParams();
-  state.forEach((value, key) => {
-    searchParams.append(key, value);
-  });
-  const origin = globalThis.location.origin;
-  const pathname = globalThis.location.pathname;
-  const search = searchParams.toString();
-  globalThis.location.href = `${origin}${pathname}?${search}`;
+type StateType = Map<string, string | string[]>;
+
+class URLState {
+  private separator: string;
+
+  constructor(seperator = ",") {
+    this.separator = seperator;
+  }
+
+  set(state: StateType): void {
+    const searchParams = new URLSearchParams();
+    state.forEach((value, key) => {
+      if (Array.isArray(value)) {
+        searchParams.append(key, value.join(this.separator));
+      } else {
+        searchParams.append(key, value);
+      }
+    });
+    const newURL = new URL(globalThis.location.href);
+    newURL.search = searchParams.toString();
+    globalThis.history.pushState({ path: newURL.href }, "", newURL.href);
+  }
+
+  get(): StateType {
+    const search = globalThis.location.search;
+    const searchParams = new URLSearchParams(search);
+    const state: StateType = new Map();
+    searchParams.forEach((value, key) => {
+      if (value.includes(this.separator)) {
+        state.set(key, value.split(this.separator));
+      } else {
+        state.set(key, value);
+      }
+    });
+    return state;
+  }
 }
 
-function get(): Map<string, string> {
-  const search = globalThis.location.search;
-  const searchParams = new URLSearchParams(search);
-  const state = new Map<string, string>();
-  searchParams.forEach((value, key) => {
-    state.set(key, value);
-  });
-  return state;
-}
-
-export { set, get };
+export { URLState };
