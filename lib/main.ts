50f2fffc4a173bb76fc13f194698e0ff1ba3aab1
Cédric Agoliki <agolikicedric@gmail.com>
2023-10-08 09:13:44 +0000

    remove class implementation, because multiple instance of this is not very useful

--- lib/main.ts
+++ lib/main.ts
@@ -1,39 +1,33 @@
 type StateType = Map<string, string | string[]>;
 
-class URLState {
-  private separator: string;
+const separator = ",";
 
-  constructor(seperator = ",") {
-    this.separator = seperator;
-  }
+const set = (state: StateType): void => {
+  const searchParams = new URLSearchParams();
+  state.forEach((value, key) => {
+    if (Array.isArray(value)) {
+      searchParams.append(key, value.join(separator));
+    } else {
+      searchParams.append(key, value);
+    }
+  });
+  const newURL = new URL(globalThis.location.href);
+  newURL.search = searchParams.toString();
+  globalThis.history.pushState({ path: newURL.href }, "", newURL.href);
+};
 
-  set(state: StateType): void {
-    const searchParams = new URLSearchParams();
-    state.forEach((value, key) => {
-      if (Array.isArray(value)) {
-        searchParams.append(key, value.join(this.separator));
-      } else {
-        searchParams.append(key, value);
-      }
-    });
-    const newURL = new URL(globalThis.location.href);
-    newURL.search = searchParams.toString();
-    globalThis.history.pushState({ path: newURL.href }, "", newURL.href);
-  }
+const get = (): StateType => {
+  const search = globalThis.location.search;
+  const searchParams = new URLSearchParams(search);
+  const state: StateType = new Map();
+  searchParams.forEach((value, key) => {
+    if (value.includes(separator)) {
+      state.set(key, value.split(separator));
+    } else {
+      state.set(key, value);
+    }
+  });
+  return state;
+};
 
-  get(): StateType {
-    const search = globalThis.location.search;
-    const searchParams = new URLSearchParams(search);
-    const state: StateType = new Map();
-    searchParams.forEach((value, key) => {
-      if (value.includes(this.separator)) {
-        state.set(key, value.split(this.separator));
-      } else {
-        state.set(key, value);
-      }
-    });
-    return state;
-  }
-}
-
-export { URLState };
+export { get, set };
